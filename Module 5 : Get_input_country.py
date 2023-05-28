77b1623476a528ca827221edd8d576f2f3b7b8b2
mind001 <kiit.705137@gmail.com>
2023-05-28 20:53:35 +0530

    Create Module 5 : Get_input_country.py

---
+++ Module 5 : Get_input_country.py
@@ -0,0 +1,7 @@
+def get_input_country():
+    country = input("Enter your country name: ")
+    return country
+
+# Test case
+user_country = get_input_country()
+print("User's country:", user_country)
