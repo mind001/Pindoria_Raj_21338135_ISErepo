e84fe37ca5a9e372671c488ba9e0b7bb951c0476
mind001 <kiit.705137@gmail.com>
2023-05-28 21:41:04 +0530

    Create Module 9: print_output.py

---
+++ Module 9: print_output.py
@@ -0,0 +1,10 @@
+def print_output(country, month, season):
+    print("Country:", country)
+    print("Month:", month)
+    print("Season:", season)
+
+# Test case
+country = "France"
+month = 7
+season = "Summer"
+print_output(country, month, season)
